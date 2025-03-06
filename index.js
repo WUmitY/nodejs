4bef64440ebb52a188f6f484f3c378955396993c
WUmitY <umityalcintr@hotmail.com>
2025-03-06 12:30:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-var express = require("express");
-var app = express();
+//var express = require("express");
+//var app = express();
+//const path = require("path");
 
-const path = require("path");
+//app.get("/", function (req, res) {
+//  res.sendFile(path.join(__dirname, "/index.html"));
+//});
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
+//app.listen(process.env.PORT ?? 3000, function () {
+//  console.log(`Listening on port ${process.env.PORT ?? 3000}...`);
+//});
+// Express modülünü dahil et
+const express = require('express');
+const path = require('path');
+
+// Uygulama örneğini oluştur
+const app = express();
+
+// Port numarasını belirle
+const port = 3000;
+
+// Ana sayfa (start page) için route tanımla
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT ?? 3000, function () {
-  console.log(`Listening on port ${process.env.PORT ?? 3000}...`);
+// Uygulama dinlemeye başlasın
+app.listen(port, () => {
+  console.log(`Sunucu http://localhost:${port} adresinde çalışıyor...`);
 });
 const date = require('date-and-time') 
 const now1  =  new Date(); 
