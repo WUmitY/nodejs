e615740428560be82479bce9aac60afd5122e185
WUmitY <umityalcintr@hotmail.com>
2025-03-06 12:23:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ app.get("/", function (req, res) {
 app.listen(process.env.PORT ?? 3000, function () {
   console.log(`Listening on port ${process.env.PORT ?? 3000}...`);
 });
+const date = require('date-and-time') 
+const now1  =  new Date(); 
+const now2  =  new Date(); 
+const value = date.isSameDay(now1,now2); 
+// Display the result 
+if(value) 
+    console.log("Both dates are same") 
+else
+    console.log("Both dates are not same");
